7551f0f31f3a6620792b53e60bf6c51c622d20c8
Lorenzo Fiore <fiorelorenzo.fl@gmail.com>
2026-05-15 16:26:05 +0200

    chore(app): ESLint + Prettier with TS / Svelte support

---
+++ app/.eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2024,
+    sourceType: 'module',
+  },
+  env: {
+    node: true,
+    browser: true,
+    es2024: true,
+  },
+  overrides: [
+    {
+      files: ['*.svelte'],
+      parser: 'svelte-eslint-parser',
+      parserOptions: { parser: '@typescript-eslint/parser' },
+      extends: ['plugin:svelte/recommended'],
+    },
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'error',
+  },
+  ignorePatterns: ['out/', 'node_modules/', '.vite/', '*.config.ts', 'forge.config.ts'],
+};
