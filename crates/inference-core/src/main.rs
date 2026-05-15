b356b5da8b7be14f4e0c66dcfe9aeaf9f0e6f070
Lorenzo Fiore <fiorelorenzo.fl@gmail.com>
2026-05-15 16:13:51 +0200

    chore(rust): clippy pedantic with sensible allows

--- crates/inference-core/src/main.rs
+++ crates/inference-core/src/main.rs
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright 2026 Lorenzo Fiore
 
+#![warn(clippy::pedantic)]
+#![allow(clippy::missing_errors_doc, clippy::missing_panics_doc)]
+
 fn main() {
     println!("inference-core stub");
 }
